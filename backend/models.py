308727e7361dd55e7951c8c16a22259aee1f8cda
Grisha <grishaok2012@gmail.com>
2026-06-18 14:23:45 +0300

    feat: add Recipe model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, ForeignKey, JSON, Integer, DateTime
 from app.database import Base
 from datetime import datetime
 
@@ -10,3 +10,16 @@ class User(Base):
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    recipes: Mapped[list["Recipe"]] = relationship(back_populates="user")
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(300), nullable=False)
+    ingredients: Mapped[list[str]] = mapped_column(JSON, nullable=False)
+    instructions: Mapped[str] = mapped_column(String(500), nullable=False)
+    category: Mapped[str] = mapped_column(String(50), nullable=False)
+    cooking_time: Mapped[int] = mapped_column(Integer, nullable=False)
+    user: Mapped["User"] = relationship(back_populates="recipes")
