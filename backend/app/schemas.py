5c13d7eec953e5ecc19dd700843572d40f527c41
Grisha <grishaok2012@gmail.com>
2026-06-18 15:52:02 +0300

    feat: add UserCreate schema

---
+++ backend/app/schemas.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field, EmailStr
+
+class UserCreate(BaseModel):
+    name: str = Field(..., min_length=1, max_length=100, description="Имя пользователя")
+    email: EmailStr = Field(..., min_length=1, max_length=100, description="Почта пользователя")
+    password: str = Field(..., min_length=8, max_length=255, description="Пароль пользователя")
