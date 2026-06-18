78db8dbdf5b8b9b078531985c5b4e66134b596f4
Grisha <grishaok2012@gmail.com>
2026-06-18 22:43:08 +0300

    feat: add JWT token creation

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 from jose import jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import os
 from dotenv import load_dotenv
 
@@ -19,3 +19,10 @@ def hash_password(password: str) -> str:
 def verify_password(password: str, hashed: str) -> bool:
     is_password = pwd_context.verify(password, hashed)
     return is_password
+
+def create_access_token(user_id: int) -> str:
+    to_encode = {"sub": str(user_id)}
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return token
