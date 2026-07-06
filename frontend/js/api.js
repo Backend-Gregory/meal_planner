dbf563b82c5fb3f3ee071b2b968ea58f878204f2
Grisha <grishaok2012@gmail.com>
2026-07-06 18:48:54 +0300

    feat: add apiRequest in api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,28 @@
 const API_BASE = 'http://127.0.0.1:8000'
 
 function get_token() {
-    return localStorage.getItem('token');
+    return localStorage.getItem('token')
+}
+
+async function apiRequest(endpoint, method = 'GET', data = null) {
+    const fullURL = API_BASE + endpoint
+    const token = get_token()
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    if (token) {
+        headers['Authorization'] = `Bearer ${token}`
+    }
+
+    const config = {
+        method,
+        headers
+    };
+
+    if (data) {
+        config.body = JSON.stringify(data)
+    }
+
+    const response = await fetch(`${fullURL}`, config)
+    return response
 }
