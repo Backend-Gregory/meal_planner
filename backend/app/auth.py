55c4e24133fffa7dcc227dbd5e83c1790cf0a895
Grisha <grishaok2012@gmail.com>
2026-07-09 22:51:48 +0300

    feat: add Refresh Token support with 90-day expiration

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -6,6 +6,7 @@ from app.config import settings
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+REFRESH_TOKEN_EXPIRE_DAYS = settings.REFRESH_TOKEN_EXPIRE_DAYS
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -17,12 +18,21 @@ def verify_password(password: str, hashed: str) -> bool:
     is_password = pwd_context.verify(password, hashed)
     return is_password
 
-def create_access_token(user_id: int) -> str:
+def create_token(user_id: int, type_token: str = 'access') -> str:
     to_encode = {"sub": str(user_id)}
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    time = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if type_token == 'refresh':
+        time = timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    expire = datetime.now(timezone.utc) + time
     to_encode.update({"exp": expire})
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return token
+
+def create_access_token(user_id: int) -> str:
+    return create_token(user_id)
+
+def create_refresh_token(user_id):
+    return create_token(user_id, 'refresh')
 
 def decode_token(token: str):
     try:
--- backend/app/config.py
+++ backend/app/config.py
@@ -16,7 +16,8 @@ class Settings():
 
     SECRET_KEY: str = os.getenv("SECRET_KEY", "change_this_in_production")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+    REFRESH_TOKEN_EXPIRE_DAYS: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 90))
 
     POOL_SIZE: int = int(os.getenv("POOL_SIZE", 10))
     MAX_OVERFLOW: int = int(os.getenv("MAX_OVERFLOW", 20))
